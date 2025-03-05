eaa4841aad514ed8a6fedb44a45f7a75b026c006
orlandojulio12 <orlandojulio76@gmail.com>
2025-03-05 12:05:25 -0500

    Cambios

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const express = require('express')
+const express = require('express');
 const { getConnection } = require('./db/db-connection-mongo');
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
 
-
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 getConnection();
 
+// Rutas
 app.use('/moduloDirector', require('./routes/moduloDirector'));
 app.use('/moduloGenero', require('./routes/moduloGenero'));
 app.use('/moduloTipo', require('./routes/moduloTipo'));
+app.use('/moduloMedia', require('./routes/moduloMedia')); // Agrega esta línea
 app.use('/moduloProductora', require('./routes/moduloProductora'))
 
-
-app.listen(port, () =>{
-    console.log(`example app listening on port ${port}`)
-})
-
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
